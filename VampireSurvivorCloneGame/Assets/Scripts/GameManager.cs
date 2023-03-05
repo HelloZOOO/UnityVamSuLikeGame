313a89b6547fd213589832e34aca9ab4b6dbaf87
HelloZOOO <qnrwnd10119@gmail.com>
2023-03-05 13:04:03 +0900

    플레이타임 생성

--- VampireSurvivorCloneGame/Assets/Scripts/GameManager.cs
+++ VampireSurvivorCloneGame/Assets/Scripts/GameManager.cs
@@ -6,15 +6,29 @@ public class GameManager : MonoBehaviour
 {
     //정적변수 static을 미리 설정해두면 즉시 클래스에서 부를 수 있다는 편리함이 있다
     public static GameManager instance;
+
+    //게임시간과 최대게임시간을 담당할 변수
+    public float gameTime; //실제 게임타임
+    public float maxGameTime = 2 * 10f; //최대 게임타임 (숫자는 '초')
+
+
     public PoolManager pool;
     public Player player;
 
     void Awake() 
     {
         instance = this;
-        /*
-        instance = GetComponent<GameManager>();
-        player = GetComponent<Player>();
-        */
+    }
+
+    void Update()
+    {
+        gameTime += Time.deltaTime;
+
+        //현재 플레이타임이 최대 플레이타임을 초과한다면
+            //현재 게임시간을 최대 게임시간으로 고정시킵니다
+        if(gameTime > maxGameTime)
+        {
+            gameTime = maxGameTime;
+        }
     }
 }
--- VampireSurvivorCloneGame/Assets/Scripts/Spawner.cs
+++ VampireSurvivorCloneGame/Assets/Scripts/Spawner.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public Transform[] spawnPoint;
+    int level; //게임 난이도를 결정한 레벨설정
     float timer;
     void Awake() 
     {
@@ -15,10 +16,14 @@ public class Spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //gamTime을 특정 초마다 나눠서 레벨이 올라가게 할것이다 
+        //ex) 50초가 흘렀고 10초마다 레벨이 올라가면 현재 레벨은 5
+        //FloorToInt를 써서 소수점의 나머지를 없앤다
         timer += Time.deltaTime;
+        level = Mathf.FloorToInt(GameManager.instance.gameTime / 10f);
 
         //타이머가 0.2f 초마다 몬스터 소환
-        if(timer > 0.2f)
+        if(timer > (level == 0 ? 0.5f : 0.2f))
         {
             timer = 0;
             Spawn();
@@ -27,11 +32,10 @@ public class Spawner : MonoBehaviour
 
     void Spawn()
     {
-        //n번은 Enemy(n)
-        //Random 0,2 를 해줘야 0~1 둘 중 하나를 선택함
         //Instantiate 반환 값을 enemy변수에 넣어둡니다 재활용 할것이기 때문입니다
         //아래 코드에서 Random.Range가 1부터 시작하는 이유는12번줄에 겟컴포넌트를 할때 스스로도 포함하기때문에 0이 아니라 2로 시작해야한다
-        GameObject enemy =  GameManager.instance.pool.Get(Random.Range(0,2));
-        enemy.transform.position = spawnPoint[Random.Range(1,spawnPoint.Length)].position;
+        //레벨에따라 바뀌는 몬스터 1레벨일땐 Enemy1 2레벨일땐 Enemy2... n초마다 등장하는 몬스터가 달라진다
+        GameObject enemy =  GameManager.instance.pool.Get(level);
+        enemy.transform.position = spawnPoint[Random.Range(1,spawnPoint.Length)].position; //몬스터 생성 위치
     }
 }
