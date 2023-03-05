d88d91091af88b4c1a236b04ef36d4beb67e0252
HelloZOOO <qnrwnd10119@gmail.com>
2023-03-05 15:16:14 +0900

    스폰데이터 구현하여 몬스터 소환

--- VampireSurvivorCloneGame/Assets/Scripts/Enemy.cs
+++ VampireSurvivorCloneGame/Assets/Scripts/Enemy.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float speed; //몬스터 이속
+    public float health; //몬스터의 현체력
+    public float maxHealth;
+    public RuntimeAnimatorController[] animCon; //애니메이션 컨트롤러를 각 몬스터 타입에 맞게 넣기위해 배열로 선언
     public Rigidbody2D target; //물리적으로 따라갈거기때문에 리지드바디를 타입으로 둠
+    public Animator anim;
     public bool isLive; //살았는지 죽었는지 확인함
     Rigidbody2D rigid; //내위치(몬스터위치)
     SpriteRenderer spriter;
@@ -13,6 +17,7 @@ public class Enemy : MonoBehaviour
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         spriter = GetComponent<SpriteRenderer>();
     }
 
@@ -45,5 +50,24 @@ public class Enemy : MonoBehaviour
         //게임매니저에 이미 플레이어가 저장되어있기떄문에 타겟을 저장할 수 있음
         //target의 Type은 Rigidbody2D라 플레이어 내부에 Rigidbody2D를 불러와야한다
         target = GameManager.instance.player.GetComponent<Rigidbody2D>();
+        isLive = true;
+        health = maxHealth; //objectPooling이 일어났을때 피를 원래대로 되돌리기 위해서 maxHealth를 넣어줌
+    }
+
+    //스폰데이터를 그대로 때려박음
+    //변수 data에 SpawnData 스크립트를 넣음으로서 SpawnData에 접근할 수 있음
+    public void Init(SpawnData data)
+    {
+        // ... 스프라이트 & 애니메이션 변경부분
+        //스프라이트의 타입은 인덱스로만 쓴다 (어처피 배열이 애니메이터랑 동일하니까)
+        //애니메이터의 컨트롤을 뭐로바꿀래? animCon[스프라이트배열] 로 바꿀게여!
+        anim.runtimeAnimatorController = animCon[data.spriteType];
+
+        // ... 스피드부분
+        speed = data.speed;
+
+        // ... 피통부분
+        maxHealth = data.health;
+        health = data.health; //최대피통이 올라갔기때문에 현피통도 초기화해준다
     }
 }
--- VampireSurvivorCloneGame/Assets/Scripts/Spawner.cs
+++ VampireSurvivorCloneGame/Assets/Scripts/Spawner.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public Transform[] spawnPoint;
+    public SpawnData[] spawnData;
     int level; //게임 난이도를 결정한 레벨설정
     float timer;
     void Awake() 
@@ -20,10 +21,13 @@ public class Spawner : MonoBehaviour
         //ex) 50초가 흘렀고 10초마다 레벨이 올라가면 현재 레벨은 5
         //FloorToInt를 써서 소수점의 나머지를 없앤다
         timer += Time.deltaTime;
-        level = Mathf.FloorToInt(GameManager.instance.gameTime / 10f);
+        level = Mathf.Min(Mathf.FloorToInt(GameManager.instance.gameTime / 10f), spawnData.Length - 1);
 
+        //레벨이 0일떈 0.5초마다 소환합니다
         //타이머가 0.2f 초마다 몬스터 소환
-        if(timer > (level == 0 ? 0.5f : 0.2f))
+        //스폰데이터 속 레벨에따라 스폰시간 결정
+        //근데 여기엔 레벨(1~2 값이 들어오는데 여기서 10을 나눠버리면 몬스터가 너무 많이 스폰되잖아)
+        if(timer > spawnData[level].spawnTime / 10f)
         {
             timer = 0;
             Spawn();
@@ -35,7 +39,20 @@ public class Spawner : MonoBehaviour
         //Instantiate 반환 값을 enemy변수에 넣어둡니다 재활용 할것이기 때문입니다
         //아래 코드에서 Random.Range가 1부터 시작하는 이유는12번줄에 겟컴포넌트를 할때 스스로도 포함하기때문에 0이 아니라 2로 시작해야한다
         //레벨에따라 바뀌는 몬스터 1레벨일땐 Enemy1 2레벨일땐 Enemy2... n초마다 등장하는 몬스터가 달라진다
-        GameObject enemy =  GameManager.instance.pool.Get(level);
+        //스폰데이터에서 관리하기때문에 이제 몬스터타입이 하나만 있어 0을 넣습니다
+        GameObject enemy =  GameManager.instance.pool.Get(0);
         enemy.transform.position = spawnPoint[Random.Range(1,spawnPoint.Length)].position; //몬스터 생성 위치
+        
+        //겟 컴포넌트에 내가 작성한 스크립트를 불러와서 그 안에있는 함수를 꺼내 쓸 수 있다
+        enemy.GetComponent<Enemy>().Init(spawnData[level]);
     }
+}
+
+[System.Serializable]
+public class SpawnData
+{
+    public float spawnTime; //스폰타임
+    public int spriteType; //스프라이트타입이 0일땐 해골 1일땐 좀비 이렇게 바뀌게 할것임
+    public int health; //몬스터 피통
+    public float speed; //몬스터 이속
 }
